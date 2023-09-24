7b3e65889b0e5bbe03e2770d089df36d4d5308e9
Natasha Desai <natashadesai@natashas-mbp.mynetworksettings.com>
2023-09-24 13:39:01 -0400

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Asset Allocation Calculator
+
+Enter a number into the 'Investable Assets' input. The calculator will return the number of Bitcoin and Ethereum that can be purchased with that value. 
+
+## To run locally
+
+In the project directory, run:
+
+### `npm i`
+
+Followed by:
+
+### `npm start`
+
+This runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Input, Select, Row, Col, Card } from "antd";
+import axios from "axios";
+
+function App() {
+  const [rates, setRates] = useState({});
+  const [investable, setInvestable] = useState();
+  const [allocation, setAllocation] = useState({BTC:0, ETH:0});
+
+  useEffect(() => {
+    axios.get("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+      .then(res => {
+        let temp = {};
+        for (let curr in res.data.data.rates) {
+          if (["BTC", "ETH"].includes(curr)) {
+            temp[curr] = res.data.data.rates[curr];
+          }
+        }
+        setRates(temp)
+      })
+  }, [])
+
+  function handleChange(e) {
+    let val = e.target.value.replace(/[^0-9]/g, '');
+    val = parseInt(val.replace(/\D/g, ''))/100;
+    val = isNaN(val) ? 0 : val;
+    !val ? setAllocation({BTC:0, ETH:0}) : setAllocation({ BTC: (val * .7 * rates.BTC).toFixed(8), ETH: (val * .3 * rates.ETH).toFixed(8), });
+    setInvestable(val.toLocaleString());
+  }
+
+  return (
+    <div>
+      <Card bordered={false} title="Asset Allocation Calculator" className="top-2 mw5 mw6-ns center pa3 flex-column" id="main">
+        <div className="f5 pt2 flex items-end">
+          <div className="pr2">Investable Assets:</div>
+          <Input value={investable} className="mw5 f5" placeholder="0.00" onChange={handleChange} id="investable" prefix="$" suffix="USD"/>
+          </div>
+        <div className="f5 pv4 flex">
+          70% BTC Allocation:
+          <div className="b pl2">{allocation.BTC}</div>
+        </div>
+        <div className="f5 flex">
+          30% ETH Allocation:
+          <div className="b pl2">{allocation.ETH}</div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
